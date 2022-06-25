08bc43acb55db931ff506f49fa0e14b10e57fa91
Brian Crossley <bcrossley712@gmail.com>
2022-06-24 18:10:46 -0600

    now we're getting somewhere

--- Planner.client/src/pages/HomePage.vue
+++ Planner.client/src/pages/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
-      <div class="col-10 offset-1 bg-white rounded elevation-3 shadow">
-        <div class="row">
+    <div class="row p-5">
+      <div
+        class="col-10 offset-1 bg-white rounded elevation-3 shadow project-card"
+      >
+        <div class="row pb-4">
           <div class="col-12 d-flex justify-content-between align-items-center">
             <div class="d-flex flex-column">
               <h-5 class="text-primary fw-bold">Projects</h-5>
@@ -22,10 +24,20 @@
             Started
           </div>
         </div>
-        <div class="row">
-          <div class="col-4"></div>
-          <div class="col-4"></div>
-          <div class="col-4"></div>
+        <div
+          class="row py-2 selectable align-items-center"
+          v-for="p in projects"
+          :key="p.id"
+        >
+          <div class="col-4 fw-bold">{{ p.name }}</div>
+          <div class="col-4">
+            <img
+              class="img-small rounded-circle"
+              :src="p.creator?.picture"
+              alt="creator image"
+            />
+          </div>
+          <div class="col-4">{{ p.createdAt }}</div>
         </div>
       </div>
     </div>
@@ -33,12 +45,37 @@
 </template>
 
 <script>
+import { computed, onMounted } from "@vue/runtime-core"
+import { projectsService } from "../services/ProjectsService";
+import { logger } from "../utils/Logger";
+import Pop from "../utils/Pop";
+import { AppState } from "../AppState";
 export default {
-  name: 'Home'
+  setup() {
+    onMounted(async () => {
+      try {
+        await projectsService.getProjects()
+      } catch (error) {
+        logger.error(error)
+        Pop.toast(error.message, 'error')
+      }
+    }
+    )
+    return {
+      projects: computed(() => AppState.projects)
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+.img-small {
+  height: 50px;
+}
+.project-card {
+  min-height: 80vh;
+  padding: 4rem;
+}
 .home {
   display: grid;
   height: 80vh;
---
+++ Planner.client/src/services/SprintsService.js
@@ -0,0 +1,3 @@
+class SprintsService {
+}
+export const sprintsService = new SprintsService();
